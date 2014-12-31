763dc9cad4d5eec44f7840e7da9c492008d02153
Florian Nussberger <florian.nussberger@gmail.com>
2014-12-30 16:12:45 -1000

    First steps at a message ticker.

--- RadioClockViewController.fs
+++ RadioClockViewController.fs
@@ -20,23 +20,28 @@ type RadioClockViewController() =
 
         let clock = new Clock()
         let toolbar = new Toolbar (clock)
+        let ticker = new Ticker()
         let weather = new WeatherStation()
 
         this.View.BackgroundColor <- UIColor.Black
         this.View.AddSubview (clock)
+        this.View.AddSubview (ticker)
         this.View.AddSubview (toolbar)
   
 
         let metrics = new NSDictionary()
-        let views = new NSDictionary("toolbar", toolbar, "clock", clock)
-        let ch0 = NSLayoutConstraint.FromVisualFormat("H:|[toolbar]|", NSLayoutFormatOptions.DirectionLeadingToTrailing, metrics, views) 
-        let ch1 = NSLayoutConstraint.FromVisualFormat("H:|[clock]|", NSLayoutFormatOptions.DirectionLeadingToTrailing, metrics, views) 
-        let cv = NSLayoutConstraint.FromVisualFormat("V:|[clock][toolbar(44)]|", NSLayoutFormatOptions.DirectionLeadingToTrailing, metrics, views) 
+        let views = new NSDictionary("toolbar", toolbar, "ticker", ticker, "clock", clock)
+        let ch0 = NSLayoutConstraint.FromVisualFormat("H:|[clock]|", NSLayoutFormatOptions.DirectionLeadingToTrailing, metrics, views) 
+        let ch1 = NSLayoutConstraint.FromVisualFormat("H:|[ticker]|", NSLayoutFormatOptions.DirectionLeadingToTrailing, metrics, views) 
+        let ch2 = NSLayoutConstraint.FromVisualFormat("H:|[toolbar]|", NSLayoutFormatOptions.DirectionLeadingToTrailing, metrics, views) 
+        let cv = NSLayoutConstraint.FromVisualFormat("V:|[clock][ticker(44)][toolbar(44)]|", NSLayoutFormatOptions.DirectionLeadingToTrailing, metrics, views) 
         this.View.AddConstraints(ch0)
         this.View.AddConstraints(ch1)
+        this.View.AddConstraints(ch2)
         this.View.AddConstraints(cv)
 
         clock.Start()
+        ticker.Text <- "Hello world.....       This is a message....."
 
     // Return true for supported orientations
     override this.ShouldAutorotateToInterfaceOrientation(orientation) = true
---
+++ Ticker.fs
@@ -0,0 +1,50 @@
+﻿namespace RadioClock
+
+open System
+open System.Drawing
+open MonoTouch.CoreAnimation
+open MonoTouch.CoreGraphics
+open MonoTouch.Foundation
+open MonoTouch.UIKit
+
+type Message() as m =
+    inherit UILabel()
+
+    do
+        m.TranslatesAutoresizingMaskIntoConstraints <- false  // important for auto layout!
+        //m.Font <- font
+        //m.Text <- digit
+        m.TextAlignment <- UITextAlignment.Left
+        //m.BackgroundColor <- new UIColor(0.0f, 0.0f, 0.0f, 0.0f)
+        m.TextColor <- UIColor.Black
+
+type Ticker() as t = 
+    inherit UIView()
+
+    let message = new Message()
+
+    let scrollOut() =
+        UIView.Animate(5.0, 5.0, UIViewAnimationOptions.CurveLinear, new NSAction(fun () -> message.Center <- new PointF (message.Center.X - 1000.0f, message.Center.Y)), null)
+
+    let scrollIn() =
+        let center = message.Center
+        message.Center <- new PointF (message.Center.X + 1000.0f, message.Center.Y)
+        UIView.Animate(5.0, 0.0, UIViewAnimationOptions.CurveLinear, new NSAction(fun () -> message.Center <- center), new NSAction(fun () -> scrollOut()))
+
+    let animate text = 
+        message.Text <- text
+        scrollIn()
+
+    do
+        t.TranslatesAutoresizingMaskIntoConstraints <- false  // important for auto layout!
+        t.BackgroundColor <- UIColor.White
+        t.AddSubview(message)
+        let metrics = new NSDictionary()
+        let views = new NSDictionary("m", message)
+        let h = NSLayoutConstraint.FromVisualFormat("H:|[m]|", NSLayoutFormatOptions.DirectionLeadingToTrailing, metrics, views) 
+        let v = NSLayoutConstraint.FromVisualFormat("V:|[m]|", NSLayoutFormatOptions.DirectionLeadingToTrailing, metrics, views) 
+        t.AddConstraints(h)
+        t.AddConstraints(v)
+
+    member t.Text with set(text) = animate(text)
+
