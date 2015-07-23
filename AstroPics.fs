f56108754f3e356f669bd0f899af0ebd2f6dbe08
Florian Nussberger <florian.nussberger@gmail.com>
2015-07-23 14:53:59 +0200

    Code cleanup and simplifications.

--- AstroPics.fs
+++ AstroPics.fs
@@ -6,56 +6,68 @@ open System.Text.RegularExpressions
 open MonoTouch.UIKit
 open MonoTouch.Foundation
 
-// rumors have it using Xamarin.Forms would make this a tad bit simpler... this is how we do it without
-// RSS http://www.acme.com/jef/apod/rss.xml
+/// Provides an endless stream of background images.
+/// The images are taken from an rss feed which contains url links the most recent astronomy pictures
+/// of the day in the description of the new items. See also http://apod.nasa.gov/apod/astropix.html.
 type AstroPics(view: UIImageView) =
 
-    let webClient   = new WebClient()    
-    let timer       = new System.Timers.Timer(10000.0)
-    let mutable nxtImage: UIImage = null
-    let mutable images: list<string> = List.empty
-    let mutable img = 0
+    let AstroRssFeed = "http://www.acme.com/jef/apod/rss.xml"
 
-    let updatePicture(): Unit =
-        // todo: nxtImage could be null here..
+    /// Displays an image in the background.
+    let updatePicture (img: UIImage) : Unit =
         view.InvokeOnMainThread(fun _ ->
-            using (nxtImage) (fun _ ->
-                UIView.Transition(view, 3.0, UIViewAnimationOptions.TransitionCrossDissolve, (fun _ -> view.Image <- nxtImage), null)
-            )
+            UIView.Transition(view, 3.0, UIViewAnimationOptions.TransitionCrossDissolve, (fun _ -> view.Image <- img), null)
         )
 
-    let loadPicItems(): Unit = 
-        // execute load in background and add news items once they are available
-        let newestItems = RssFeed.items("http://www.acme.com/jef/apod/rss.xml") 
-        ()
-        //lock nitems (fun _ -> nitems <- nitems |> List.append(newestItems))
+    /// Downloads a picture.
+    let nextPicture (url: String) : option<UIImage> = 
+        try
+            use webClient = new WebClient()    
+            let bytes     = webClient.DownloadData(new Uri(url))
+            Some(new UIImage(NSData.FromArray(bytes)))
+        with
+            | _ -> None
 
-
-
-    let imageSource (input: string): list<string> =
+    /// Gets a single image url from the given string. 
+    let urlFromDesc (input: string) : list<string> =
         let m = Regex.Match(input, "img src=\"([^\"]*)") 
-        if (m.Success) then [m.Groups.Item(1).Value] else [] // group 0 is the whole matched expression, first group is 1
+        // group 0 is the whole matched expression, first group is 1
+        if (m.Success) then [m.Groups.Item(1).Value] else [] 
 
-    let imageSources (items: list<NewsItem>): list<string> =   
-        List.map (fun i -> imageSource(i.desc)) items |> List.concat // flatten list in case regexp didn't match
+    /// Gets a batch of background image urls from the items in an RSS feed.
+    let nextUrls () : list<string> =   
+        let items = RssFeed.items(AstroRssFeed)
+        List.map (fun i -> urlFromDesc(i.desc)) items 
+        |> List.concat // flatten list in case regexp didn't match
 
-    let nextPicture(): Unit =
-        img <- img + 1  // TODO: need error handling in case Download fails, to be sure inc first
-        updatePicture()
-        webClient.DownloadDataAsync(new Uri(List.nth images (img % images.Length)))  // TODO fails if images.Length is 0
+    /// Creates an endless sequence of background image urls.
+    let imageUrlSeq : seq<option<string>> =
+        Seq.unfold (fun urls ->
+            match urls with                                 // consume current batch of urls
+                | u::us -> 
+                    Some(Some u, us)
+                | []    ->
+                   match nextUrls() with                    // once we run out, load a fresh batch of urls
+                    | u::us -> Some(Some u, us)
+                    | []    -> Some(None,   nextUrls())     // (fresh batch can be empty in case of an error)
+        ) []
 
-    let pictureLoaded (bytes: byte[]): Unit =
-        if (bytes.Length > 0) then
-            nxtImage <- new UIImage(NSData.FromArray(bytes))
+    /// Creates an endless sequence of background images.
+    let imageSeq : seq<option<UIImage>> =
+        Seq.map (fun url ->
+            match url with
+                | Some u -> nextPicture(u)
+                | None   -> None
+        ) imageUrlSeq
 
     do
-        // get pictures // TODO: update pics
-        let items = RssFeed.items("http://www.acme.com/jef/apod/rss.xml")
-        images <- imageSources(items)
+        Async.Start (async {
+            let images = imageSeq.GetEnumerator()
+            while true do
+                if (images.MoveNext()) then
+                    match images.Current with
+                        | Some i -> updatePicture(i)
+                        | None   -> ()
+                do! Async.Sleep 10000
+        }) |> ignore
 
-        // install callback
-        webClient.DownloadDataCompleted.Add(fun s -> pictureLoaded(s.Result))
-
-        // show a first picture and start timer
-        timer.Elapsed.Add(fun _ -> nextPicture())
-        timer.Start()
